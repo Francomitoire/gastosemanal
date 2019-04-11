5aa0acbda8ae048c9bdd91fd339ca0ed1fd6f016
Franco <mitoirefranco@gmail.com>
2019-04-11 10:10:20 -0300

    Updates

---
+++ precache-manifest.2d734fbe6f1337def2a921a6a2bd333f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e2fc4b7d77a9c53df47",
+    "url": "/repository-name/static/js/runtime~main.f7634681.js"
+  },
+  {
+    "revision": "e0354f782f5edb2613b2",
+    "url": "/repository-name/static/js/main.0807723e.chunk.js"
+  },
+  {
+    "revision": "efe34edcc5247de89a8b",
+    "url": "/repository-name/static/js/2.dc155f86.chunk.js"
+  },
+  {
+    "revision": "e0354f782f5edb2613b2",
+    "url": "/repository-name/static/css/main.c03587ac.chunk.css"
+  },
+  {
+    "revision": "38e34fbc49f0678fe28796988e48dcf6",
+    "url": "/repository-name/index.html"
+  }
+];
